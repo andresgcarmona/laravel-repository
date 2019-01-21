12aded1ae4197f2323af23771c6c53f1cab6fef0
Andres G. Carmona <ing.andres.carmona@gmail.com>
2019-01-21 15:04:17 -0600

    Added RepositoryServiceProvider.

---
+++ src/RepositoryServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+    
+    namespace Polaris;
+    
+    use Illuminate\Support\ServiceProvider;
+    
+    class RepositoryServiceProvider extends ServiceProvider
+    {
+        /**
+         * Bootstrap services.
+         *
+         * @return void
+         */
+        public function boot()
+        {
+            //
+        }
+        
+        /**
+         * Register services.
+         *
+         * @return void
+         */
+        public function register()
+        {
+            //
+        }
+    }
